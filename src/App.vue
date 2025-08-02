89364547986294cd768849e5fb713651b436c9e9
iChera087 <ichera087@gmail.com>
2025-08-02 12:40:30 +0300

    roflo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+  function yes() { 
+    const otevet = prompt("5 мнут тебе даю понял?", ' ')
+
+    if (otevet !== 'да' && otevet !== 'yes' && otevet !== 'Да') { 
+      alert('не ты вообще прихуеваший')
+      return 
+    }
+    else { 
+      alert('молодец')
+    }
+
+    return 
+  }
+
+  function no () { 
+    alert ("ахуел, давно не пиздили?")
+    return 
+  }
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text">В кс пойдешь?</h1>
+    <button class="yes" @click="yes"> yes my bro</button>
+    <button class="no" @click="no"> no sorry</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
